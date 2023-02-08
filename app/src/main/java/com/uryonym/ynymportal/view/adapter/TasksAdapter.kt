7fe255dde88b07027a905ff9f45c5e7db8966bc0
yoneyama ryo <uryonym@gmail.com>
2023-02-08 19:21:35 +0900

    タスク一覧のレイアウトファイルをTextiViewからViewに修正

--- app/src/main/java/com/uryonym/ynymportal/view/adapter/TasksAdapter.kt
+++ app/src/main/java/com/uryonym/ynymportal/view/adapter/TasksAdapter.kt
@@ -1,13 +1,14 @@
 package com.uryonym.ynymportal.view.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.uryonym.ynymportal.R
 import com.uryonym.ynymportal.data.Task
 
-class TasksAdapter : RecyclerView.Adapter<TaskItemViewHolder>() {
+class TasksAdapter : RecyclerView.Adapter<TasksAdapter.ViewHolder>() {
     var data = listOf<Task>()
         set(value) {
             field = value
@@ -16,17 +17,19 @@ class TasksAdapter : RecyclerView.Adapter<TaskItemViewHolder>() {
 
     override fun getItemCount(): Int = data.size
 
-    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.title
+        holder.taskTitle.text = item.title
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.task_item, parent, false) as TextView
+        val view = layoutInflater.inflate(R.layout.task_item, parent, false)
 
-        return TaskItemViewHolder(view)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val taskTitle: TextView = itemView.findViewById(R.id.task_title)
     }
 }
-
-class TaskItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
